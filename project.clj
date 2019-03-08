e8eaba24295c4eca10b6e80b91d3fd17dc2fa139
fbielejec <fbielejec@gmail.com>
2019-03-08 15:59:22 +0100

    version++

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-ui-component-form "0.2.1-SNAPSHOT"
+(defproject district0x/district-ui-component-form "0.2.2-SNAPSHOT"
   :description "district UI forms library "
   :url "https://github.com/district0x/district-ui-component-form"
   :license {:name "Eclipse Public License"
