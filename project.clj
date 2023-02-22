531e6cc2e77a6db83c56c0f99a41f96e67de8cfe
ginesdt <ginesdt@gmail.com>
2023-02-22 12:13:53 +0100

    Fix file-accept-pred when not using promises

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject district0x/district-ui-component-form "0.2.14-SNAPSHOT"
+(defproject district0x/district-ui-component-form "0.2.15-SNAPSHOT"
   :description "district UI forms library "
   :url "https://github.com/district0x/district-ui-component-form"
   :license {:name "Eclipse Public License"
